0ca862b7b75a89abf4e13eb1c8fc5277a2797079
Alexandra Hájková <alexandra.khirnova@gmail.com>
2017-06-10 14:44:46 +0000

    ppc: Add vpx_idct4x4_16_add_vsx
    Change-Id: Id2673eece32027fb245919c7a5c81994a4a19fd8

--- test/fdct4x4_test.cc
+++ test/fdct4x4_test.cc
@@ -508,4 +508,11 @@ INSTANTIATE_TEST_CASE_P(
         make_tuple(&vp9_fht4x4_msa, &vp9_iht4x4_16_add_msa, 2, VPX_BITS_8),
         make_tuple(&vp9_fht4x4_msa, &vp9_iht4x4_16_add_msa, 3, VPX_BITS_8)));
 #endif  // HAVE_MSA && !CONFIG_VP9_HIGHBITDEPTH && !CONFIG_EMULATE_HARDWARE
+
+#if HAVE_VSX && !CONFIG_VP9_HIGHBITDEPTH && !CONFIG_EMULATE_HARDWARE
+INSTANTIATE_TEST_CASE_P(VSX, Trans4x4DCT,
+                        ::testing::Values(make_tuple(&vpx_fdct4x4_c,
+                                                     &vpx_idct4x4_16_add_vsx, 0,
+                                                     VPX_BITS_8)));
+#endif  // HAVE_VSX && !CONFIG_VP9_HIGHBITDEPTH && !CONFIG_EMULATE_HARDWARE
 }  // namespace
